29933f3980a4a15428a5d482320c405cfdf66631
Josef Duchesne <josef@cnmississaugawest.ca>
2021-07-05 16:08:03 -0400

    Day 2.2

--- main.py
+++ main.py
@@ -8,19 +8,37 @@ pygame.init()
 # Non-adjustable constants
 WINDOW_WIDTH = 1100 
 WINDOW_HEIGHT = 600 
+TILE_SIZE = 100
+
+# Adjustable constants
+WHITE = (255, 255, 255) 
+
+DRAW_GRID = True
 
 # Set up the window with a size and name
 GAME_WINDOW = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) 
 display.set_caption('Attack of the Vampire Pizzas!') 
 
-# Let's quickly get some stuff on the screen, load the images first:
-background_img = image.load('restaurant.jpg') 
-background_surf = Surface.convert_alpha(background_img) 
-BACKGROUND = transform.scale(background_surf, (WINDOW_WIDTH, WINDOW_HEIGHT)) 
+# load an image, if only a name is provided, assume that it is a square tile (because most objects in this game are)
+def load_img(filename, width=TILE_SIZE, height=TILE_SIZE):
+    img = image.load(filename)
+    surf = Surface.convert_alpha(img)
+    return transform.scale(surf, (width, height))
 
-pizza_img = image.load('vampire.png') 
-pizza_surf = Surface.convert_alpha(pizza_img) 
-VAMPIRE_PIZZA = transform.scale(pizza_surf, (100, 100)) 
+# Get the Rect() object corresponding to a given row and column
+def rect_from_position(row, col):
+    return Rect(TILE_SIZE * col, TILE_SIZE * row, TILE_SIZE, TILE_SIZE)
+
+# Load all of the assets
+BACKGROUND = load_img('restaurant.jpg', WINDOW_WIDTH, WINDOW_HEIGHT)
+VAMPIRE_PIZZA = load_img('vampire.png')
+
+# Set up the game tiles
+for row in range(6):
+    for column in range(11):
+        # IF we want the grid overlay (as decided by the DRAW_GRID constant), draw a box around some of the tiles
+        if DRAW_GRID: 
+                draw.rect(BACKGROUND, WHITE, rect_from_position(row, column), 1)  
 
 # Track the game state with a boolean. game_running means we are playing
 game_running = True 
@@ -31,7 +49,7 @@ while game_running:
             # If we're trying to close the window, both the game and program stop
             game_running = False
     
-    # Actually draw the things
+    # Draw some stuff
     GAME_WINDOW.blit(BACKGROUND, (0, 0)) 
     GAME_WINDOW.blit(VAMPIRE_PIZZA, (900, 400)) 
 
