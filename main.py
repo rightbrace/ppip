bf97eb0e9e34a5206bae067fc52ceb0359968610
Josef Duchesne <josef@cnmississaugawest.ca>
2021-07-07 08:55:51 -0400

    Day 3.1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import the needed libraries
 import pygame
 from pygame import * 
+from random import randint
 
 # Start pygame
 pygame.init() 
@@ -12,6 +13,11 @@ TILE_SIZE = 100
 
 # Adjustable constants
 WHITE = (255, 255, 255) 
+
+SPAWN_RATE = 360 
+
+REG_SPEED = 2 
+SLOW_SPEED = 1 
 
 DRAW_GRID = True
 
@@ -33,12 +39,28 @@ def rect_from_position(row, col):
 BACKGROUND = load_img('restaurant.jpg', WINDOW_WIDTH, WINDOW_HEIGHT)
 VAMPIRE_PIZZA = load_img('vampire.png')
 
+# VampireSprite is the main opponent. This class sets up a vampire, updates its logic, and draw it
+class VampireSprite(sprite.Sprite): 
+    def __init__(self): 
+        super().__init__() 
+        self.speed = REG_SPEED 
+        self.image = VAMPIRE_PIZZA.copy() 
+        y = 50 + randint(0, 4) * 100 
+        self.rect = self.image.get_rect(center = (WINDOW_WIDTH, y)) 
+
+    def update(self, game_window): 
+        # Draw it
+        game_window.blit(self.image, (self.rect.x, self.rect.y)) 
+
+# Here, we set up all of the game elements, and store them in varibles
+all_vampires = sprite.Group() 
+
 # Set up the game tiles
 for row in range(6):
     for column in range(11):
         # IF we want the grid overlay (as decided by the DRAW_GRID constant), draw a box around some of the tiles
         if DRAW_GRID: 
-                draw.rect(BACKGROUND, WHITE, rect_from_position(row, column), 1)  
+                draw.rect(BACKGROUND, WHITE, rect_from_position(row, column), 1)   
 
 # Track the game state with a boolean. game_running means we are playing
 game_running = True 
@@ -49,9 +71,16 @@ while game_running:
             # If we're trying to close the window, both the game and program stop
             game_running = False
     
-    # Draw some stuff
+    # Every frame, paint over the whole screen with the background
     GAME_WINDOW.blit(BACKGROUND, (0, 0)) 
-    GAME_WINDOW.blit(VAMPIRE_PIZZA, (900, 400)) 
+
+    # Very rarely, spawn a new vampire
+    if randint(1, SPAWN_RATE) == 1: 
+        all_vampires.add(VampireSprite())
+
+    # Update every vampire
+    for vampire in all_vampires: 
+        vampire.update(GAME_WINDOW)
 
     # Make the changes visible onscreen
     display.update() 
